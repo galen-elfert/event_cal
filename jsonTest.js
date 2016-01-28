0ab8caaa47232c0048c01d9654f87348ace759a9
Galen Elfert <guelfert@gmail.com>
2016-01-27 22:07:55 -0800

    first commit

---
+++ jsonTest.js
@@ -0,0 +1,28 @@
+var setupList = 
+{ "setups" : [
+    "setup01" : [
+        {   "room" : "1415", 
+            "eventNum" : 78193,
+            "eventName" : "Urban Design Economic Fundamentals URBD785", 
+            "start" : "2015-11-18T08:00",
+            "end" : "2015-11-18T13:00",
+            "building" : "Harbour Centre", 
+            "resources" : [
+                {   "quantity" : 2, 
+                    "resourceName" : "Flip Chart with paper & pens", 
+                    "setBy" : "", 
+                    "struckBy" : "",
+                    "notes" : "" },
+                {   "quantity" : 1  , 
+                    "resourceName" : "Data Projector", 
+                    "setBy" : "", 
+                    "struckBy" : "",
+                    "notes" : "" },
+                {   "quantity" : 1  , 
+                    "resourceName" : "Computer - PC, with Internet Connection", 
+                    "setBy" : "", 
+                    "struckBy" : "",
+                    "notes" : "" }
+            ]
+        }
+    ]
