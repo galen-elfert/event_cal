18e8575ed0873bf5148bbc9278ac56439a883324
Galen Elfert <guelfert@van-its-16.itds.sfu.ca>
2016-02-06 15:57:33 -0800

    finished php csv to setup object parser

--- event_exports/setup2db.php
+++ event_exports/setup2db.php
@@ -1,6 +1,23 @@
 <?php
+include "setup_classes.php";
 
-if (($inputfile = fopen("20162501-setup.csv", "r")) !== FALSE) 
+if (($infile = fopen("20162501-setup.csv", "r")) == TRUE)
+{
+    $mysetups = setup::new_setup_from_file($infile);
+    print_r($mysetups);
+} else {
+    echo "Cannot open file\n";
+}
+
+if (setup::compare($mysetups[102], $mysetups[103]))
+{
+    echo "match!\n";
+} else {
+    echo "no match\n";
+}
+
+/*
+if (($inputfile = fopen($file_name, "r")) !== FALSE)
 {
     $data = fgetcsv($inputfile);
 	    for ($i = 0; $i <= count($data); ++$i)
@@ -16,5 +33,6 @@ if (($inputfile = fopen("20162501-setup.csv", "r")) !== FALSE)
 		echo "\n".$room[0];
 		//string pattern, string subject [, array &matches [, int flags [, int offset]]])
 }
+*/
 
 ?>
--- event_exports/setup_classes.php
+++ event_exports/setup_classes.php
@@ -1,43 +1,62 @@
 <?php
 
+function hello()
+{
+    echo "Hello\n";
+}
+
 class setup
 {
     private $building, $room, $date, $time_start, $time_end, $event_number, $event_name, $resource, $quantity, $setup_by, $picked_up_by, $note;
 
-    // Private generic constructor
-
-    // Constructor
-    public static function new_setup_from_file($file_in)
+    /* Constructor for file import
+     *
+     * params:  resource $handle
+     *          file handle for csv file to parse_str
+     */
+    public static function new_setup_from_file($input_file)
     {
-        $new_setup = new setup();
-        if (($inputfile = fopen($file_in, "r")) !== FALSE) 
+        $index = 0;
+        while(($data = fgetcsv($input_file)) !== FALSE)
         {
-            while(($data = fgetcsv($inputfile)) !== FALSE)
+            $setup[$index] = new setup;
+            $setup[$index]->building = $data[0];
+            $setup[$index]->date = $data[5];
+            $event_num = substr($data[21], 1, 5);
+            if (is_numeric($event_num))
             {
-                $this->building = $data[0];
-                $this->date = $data[5];
-                $event_num = substr($data[21], 1, 5);
-                if (is_numeric($event_num) 
-                {
-                    $this->event_number = $event_num;
-                } else {
-                    // throw validation error
-                }
-	        $this->event_name = substr($data[21], 7);
-		preg_match('/(?<=Room ).+/s', $data[16], $matches);
-                $this->room = $matches[0];
-                $this->time_start = $data[14];
-                $this->time_end =  $data[15];
-                $this->resource = $data[28];
-                $this->qua
+                $setup[$index]->event_number = $event_num;
+            } else {
+                // throw validation error
             }
+            $setup[$index]->event_name = substr($data[21], 7);
+            preg_match('/(?<=Room ).+/s', $data[16], $matches);
+            $setup[$index]->room = $matches[0];
+            $setup[$index]->time_start = $data[14];
+            $setup[$index]->time_end =  $data[15];
+            $setup[$index]->resource = $data[28];
+            $setup[$index]->quantity = $data[26];
+            $index++;
         }
-        else
-        {
-            // handle bad file error
-        }
+        return $setup;
+    }
 
-        return $new_setup;
+    /* Match function
+     *
+     * Compare critical fields of two setup objects to determine if they
+     * are identical
+     */
+    public static function match(setup $a, setup $b)
+    {
+        return  $a->building    == $b->building &&
+                $a->room        == $b->room &&
+                $a->date        == $b->date &&
+                $a->event_number== $b->event_number &&
+                $a->event_name  == $b->event_name &&
+                $a->resource    == $b->resource &&
+                $a->quantity    == $b->quantity &&
+                $a->time_end    == $b->time_end &&
+                $a->time_start  == $b->time_start;
     }
 }
 ?>
